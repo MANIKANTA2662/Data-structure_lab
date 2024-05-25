6f1503271311d65e0698e32e88306e872f49bbfd
BENDI MANIKANTA <84337125+MANIKANTA2662@users.noreply.github.com>
2024-05-25 11:31:02 +0530

    Update DSlab-2.c

--- DSlab-2.c
+++ DSlab-2.c
@@ -201,7 +201,7 @@ void random_delete()
 void search()  
 {  
     struct node *ptr;  
-    int item,i=0,flag;  
+    int item,i=0,flag=-1;  
     ptr = head;   
     if(ptr == NULL)  
     {  
@@ -217,15 +217,11 @@ void search()
             {  
                 printf("item found at location %d ",i);  
                 flag=0;  
-            }   
-            else  
-            {  
-                flag=1;  
-            }  
+            }    
             i++;  
             ptr = ptr -> next;  
         }  
-        if(flag==1)  
+        if(flag==-1)  
         {  
             printf("Item not found\n");  
         }  
